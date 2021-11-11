b6ffa8ac1ffcb86f81f78913b8a81feb56f670a6
haikalgnz <haikalofc8@gmail.com>
2021-11-11 14:50:45 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -131,7 +131,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
     conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
       "listMessage": {
         "title": "Daftar Menu",
-        "description": "© stikerin",
+        "description": "© Ikal Botz",
         "buttonText": "Klik Disini",
         "listType": "SINGLE_SELECT",
         "sections": [
@@ -371,7 +371,7 @@ let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    await conn.send2Button(m.chat, text.trim(), 'made with ❤️ by ariffb', 'PEMILIK BOT', '.owner', 'DONASI', '.donasi', { quoted: m })
+    await conn.send2Button(m.chat, text.trim(), 'made with ❤️ by IkalBotz', 'PEMILIK BOT', '.owner', 'DONASI', '.donasi', { quoted: m })
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
